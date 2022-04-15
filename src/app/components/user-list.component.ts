2d9b4252ad5f5a4081bf925992e6d9802abcc559
andreaminolfi <andrea@minolfi.it>
2022-04-15 15:28:57 +0200

    2 files modified

--- src/app/components/user-list.component.ts
+++ src/app/components/user-list.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from '../model/user';
 
 @Component({
   selector: 'app-user-list',
   template: `
-    Hello List
+    <div *ngFor="let u of items" class="list-group-item d-flex justify-content-between">
+      <i class="fa fa-2x" [ngClass]="{ 'fa-mars' : u.gender === 'M', 'fa-venus': u.gender === 'F' }" [style.color]="u.gender === 'M' ? 'blue': 'pink'"> 
+      </i>
+      {{u.label}}
+      <i class="fa fa-trash" (click)="delete.emit(u)"></i>
+    </div>
   `,
 })
-export class UserListComponent {}
+export class UserListComponent {
+  @Input() items: User[] = [];
+  @Output() delete = new EventEmitter<User>();
+}
