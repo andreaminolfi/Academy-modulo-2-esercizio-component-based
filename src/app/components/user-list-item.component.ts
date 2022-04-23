681130e5258a53b851ad0223e6108d11b23f6d26
andreaminolfi <andrea@minolfi.it>
2022-04-23 21:49:09 +0200

    1 file modified

--- src/app/components/user-list-item.component.ts
+++ src/app/components/user-list-item.component.ts
@@ -8,11 +8,20 @@ import { User } from '../model/user';
       <i class="fa fa-2x" [ngClass]="{ 'fa-mars' : item.gender === 'M', 'fa-venus': item.gender === 'F' }" [style.color]="item.gender === 'M' ? 'blue': 'pink'"> 
       </i>
       {{item.label}}
-      <i class="fa fa-trash" (click)="delete.emit(item)"></i>
+      <i class="fa fa-trash" (click)="delete.emit(item)"></i><i class="fa fa-arrow-down" (click)="openClose()"></i>      
+    </div>
+    <div *ngIf="isOpen===true" class="list-group-item d-flex justify-content-between">
+      Età: {{item.age}}
     </div>
   `,
 })
 export class UserListItemComponent {
   @Input() item: User = null;
   @Output() delete = new EventEmitter<User>();
+  isOpen: boolean = false;
+
+  openClose() {
+    this.isOpen = !this.isOpen;
+    console.log(this.isOpen);
+  }
 }
